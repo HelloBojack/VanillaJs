050afd8b5e5159a0c0358e72e2d9ffa08d68e8b2
bojack <695668678@qq.com>
2021-12-13 14:28:51 +0800

    手写bind

--- 手写bind/index.js
+++ 手写bind/index.js
@@ -10,3 +10,27 @@ function fn() {
   console.log(this.name)
 }
 
+let obj = { name: 'obj' }
+let fn2 = fn.bind(obj)
+fn()
+fn2()
+
+Function.prototype.myBind = function (context, ...args) {
+  let fn = this
+  return function (...args) {
+    context.fn = fn
+    let res = context.fn(...args)
+    return res
+  }
+}
+
+Function.prototype.myBind2 = function (context, ...args) {
+  let fn = this
+  return function (...args) {
+    return fn.apply(context, args)
+  }
+}
+let fn3 = fn.myBind(obj)
+fn3()
+let fn4 = fn.myBind2(obj)
+fn4()
