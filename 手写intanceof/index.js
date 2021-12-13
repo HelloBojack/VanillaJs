1bfbd2a575ecc5600efac96bdc22f37259a1c4d9
bojack <695668678@qq.com>
2021-12-13 16:30:55 +0800

    instanceof

--- 手写intanceof/index.js
+++ 手写intanceof/index.js
@@ -4,15 +4,17 @@ console.log(arr instanceof Array)
 console.log(arr instanceof Object)
 
 function myInstanceof(obj, constructor) {
-  let proto = obj.__proto__
+  let proto = Object.getPrototypeOf(obj)
 
   while (proto) {
     if (proto == constructor.prototype) {
       return true
     }
-    proto = proto.__proto__
+    proto = Object.getPrototypeOf(proto)
   }
+  return false
 }
 
 console.log(myInstanceof(arr, Array))
 console.log(myInstanceof(arr, Object))
+console.log(myInstanceof(arr, Function))
