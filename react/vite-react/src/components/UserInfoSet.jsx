018f6bea4e81b67889eac6d0d58b352d0c0015d4
bojack <695668678@qq.com>
2021-12-17 14:41:52 +0800

    react-reducer-02

--- react/vite-react/src/components/UserInfoSet.jsx
+++ react/vite-react/src/components/UserInfoSet.jsx
@@ -1,13 +1,35 @@
 import { useContext, userState } from 'react';
 import { UserContext } from '../context/index'
-const UserInfo = () => {
+
+const reducer = (state, action) => {
+  let { type, playload } = action;
+  switch (type) {
+    case 'UPDATE':
+      return { ...state, ...playload };
+  }
+}
+
+const connect = (Component) => {
+  return () => {
+    const { userState, setUserState } = useContext(UserContext);
+    const dispatch = (action) => {
+      setUserState(reducer(userState, action))
+    }
+    return <Component dispatch={dispatch}></Component>
+  }
+}
+
+const UserInfo = ({ dispatch }) => {
   console.log('UserInfoSet.jsx render');
-  const { userState, setUserState } = useContext(UserContext);
+  const { userState } = useContext(UserContext);
   const onChange = (e) => {
-    setUserState({ ...userState, age: e.target.value })
+    // setUserState({ ...userState, age: e.target.value })
+    // setUserState(reducer(userState, { type: "UPDATE", playload: { age: e.target.value } }))
+    dispatch({ type: "UPDATE", playload: { age: e.target.value } })
   }
   return <>
     <div> User Age :<input type="text" value={userState.age} onChange={onChange} /></div>
   </>
 }
-export default UserInfo;
+
+export default connect(UserInfo);
