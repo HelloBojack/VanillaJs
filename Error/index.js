01107a8210d812eb56a006221c7e8c589b4b78e8
bojack <695668678@qq.com>
2021-12-18 23:32:10 +0800

    Error

---
+++ Error/index.js
@@ -0,0 +1,14 @@
+// console.log(a)
+// // Uncaught ReferenceError: a is not defined
+
+// let a;
+// a();
+// // Uncaught TypeError: a is not a function
+
+// let a = """"
+// // Uncaught SyntaxError: Unexpected string
+
+// (function fn() {
+//   fn()
+// })()
+// // Uncaught RangeError: Maximum call stack size exceeded
