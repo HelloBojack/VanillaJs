90781c6ca5b04349b5ed841e9e300036a4163df2
bojack <695668678@qq.com>
2021-12-19 19:20:07 +0800

    Error

--- Error/index.js
+++ Error/index.js
@@ -12,3 +12,26 @@
 //   fn()
 // })()
 // // Uncaught RangeError: Maximum call stack size exceeded
+
+try {
+  let a
+  a()
+} catch (error) {
+  console.dir(error)
+  // TypeError: a is not a function
+  // at http://127.0.0.1:5502/Error/index.js:18:3
+  // message: "a is not a function"
+  // stack: "TypeError: a is not a function\n    at http://127.0.0.1:5502/Error/index.js:18:3"
+  // [[Prototype]]: Error
+  // constructor: ƒ TypeError()
+  // message: ""
+  // name: "TypeError"
+}
+
+function fn(a) {
+  if (typeof a !== 'number') {
+    throw new TypeError('a is not a number')
+  }
+  return a
+}
+fn('1')
