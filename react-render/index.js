e62bf57ee9a848d9f98bbacdb7cac3bd979248b5
bojack <695668678@qq.com>
2021-12-23 17:43:24 +0800

    repeact

---
+++ react-render/index.js
@@ -0,0 +1,9 @@
+let vdom = {
+  type: 'h1',
+  props: {
+    className: "",
+    style: "",
+  },
+  children: [] // 嵌套节点
+}
+
--- 快手-repeat/index.js
+++ 快手-repeat/index.js
@@ -1,24 +1,34 @@
 // 手写一个repeact()函数，加上下面的代码运行，使每3秒打印一个helloword，总共执行4次
 
+// function repeact(fn, times, interval) {
+//   return (str) => {
+//     fn(str)
+//     times--
+//     let timer = setInterval(() => {
+//       fn(str)
+//       times--
+//       if (times == 0) {
+//         clearInterval(timer)
+//       }
+//     }, interval)
+//   }
+// }
+
+
 function repeact(fn, times, interval) {
-
-  return function (str) {
-    if (times > 0) {
-      fn(str);
+  return (str) => {
+    let run = setTimeout(() => {
+      fn(str)
       times--
-      let timer = setInterval(() => {
-        fn(str);
-        times--
-        if (times === 0) {
-          clearInterval(timer)
-        }
-      }, interval)
-    }
-
+      if (times == 0) {
+        clearTimeout(run)
+      } else {
+        repeact(fn, times, interval)(str)
+      }
+    }, interval)
   }
 }
 
 const repeatFunc = repeact(console.log, 4, 3000)
 repeatFunc('helloword')
-
 
