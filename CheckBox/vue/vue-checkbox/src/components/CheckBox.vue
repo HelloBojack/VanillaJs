1ca7986cd13a3f8b2302eba5d1b51759337073fe
bojack <695668678@qq.com>
2021-12-29 19:59:55 +0800

    vue checkbox

---
+++ CheckBox/vue/vue-checkbox/src/components/CheckBox.vue
@@ -0,0 +1,42 @@
+
+<template>
+ <div v-bind="info">
+   <span>全选</span>
+    <input type="checkbox" :checked="checkedAll" @change="changeCheckAll">
+ </div>
+  <div v-for="item in list" :key="item.id">
+  <label :for="item.id">
+    <input type="checkbox" v-model="checkedList" :id="item.id" :value="item.ctx">
+    {{item.ctx}}
+  </label>
+    
+  </div>
+  <div>{{checkedList}}</div>
+</template>
+<script setup>
+import { ref,reactive } from "vue";
+
+// defineProps({
+//   msg: String,
+// });
+let checkedList =ref([]);
+const checkedAll = ref(false)
+
+const info = {
+  name:'k',
+  age:25,
+}
+
+let list = reactive([
+  { id: 1, ctx: 'ctx1', checked: true },
+  { id: 2, ctx: 'ctx2', checked: false },
+  { id: 3, ctx: 'ctx3', checked: false },
+  { id: 4, ctx: 'ctx4', checked: false },
+]);
+const changeCheckAll=(e)=>{
+  console.log(e.target.checked);
+  checkedList.value=e.target.checked?list.map(item=>item.ctx):[];
+}
+
+</script>
+
