b70a3c3bb2b7f08f6fa5998761ab9d1c267af456
bojack <695668678@qq.com>
2022-02-17 16:36:23 +0800

    sor

--- 排序/插入.js
+++ 排序/插入.js
@@ -1,5 +1,11 @@
 function insertSort(arr) {
-
-
+  for (let i = 1; i < arr.length; i++) {
+    let cur = i
+    while (cur && arr[cur] < arr[cur - 1]) {
+      [arr[cur], arr[cur - 1]] = [arr[cur - 1], arr[cur]]
+      cur--
+    }
+  }
   return arr
 }
+console.log(insertSort([10, 1, 5, 6, 2, 0]));
