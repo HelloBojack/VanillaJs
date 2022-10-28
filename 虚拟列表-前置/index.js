05714fd7a95ecd6e6e9635c9f4be3ff93a64bd04
bojack <xlk970115@gmail.com>
2022-10-28 12:28:22 +0800

    虚拟列表

---
+++ 虚拟列表-前置/index.js
@@ -0,0 +1,111 @@
+const btn = document.querySelector("#button");
+const container = document.querySelector("#container");
+const total = 10000;
+const getData = () => {
+  const list = [];
+  return new Promise((reslove) => {
+    setTimeout(() => {
+      for (let i = 0; i < total; i++) {
+        list.push(i);
+      }
+      reslove(list);
+    }, 1000);
+  });
+};
+
+btn.addEventListener("click", async () => {
+  const list = await getData();
+
+  // 1. 暴力直接一个一个渲染
+  // for (let i in list) {
+  //   let li = document.createElement("li");
+  //   li.innerText = i;
+  //   container.appendChild(li);
+  // }
+
+  // 2. setTimeout 分片渲染
+  // let pageSize = 20;
+  // let pageNumber = 1;
+  // function loopRender(currCnt, pageNumber) {
+  //   if (currCnt < 0) return;
+  //   let pageCnt = Math.min(pageSize, currCnt);
+  //   setTimeout(() => {
+  //     for (let i = 0; i < pageCnt; i++) {
+  //       let li = document.createElement("li");
+  //       li.innerText = `${pageNumber}---${i}`;
+  //       container.appendChild(li);
+  //     }
+
+  //     loopRender(currCnt - pageSize, ++pageNumber);
+  //   }, 0);
+  // }
+  // loopRender(total, pageNumber);
+
+  // 2. requestAnimationFrame 分片渲染
+  // let pageSize = 20;
+  // let pageNumber = 1;
+  // function loopRender(currCnt, pageNumber) {
+  //   if (currCnt < 0) return;
+  //   let pageCnt = Math.min(pageSize, currCnt);
+  //   requestAnimationFrame(() => {
+  //     for (let i = 0; i < pageCnt; i++) {
+  //       let li = document.createElement("li");
+  //       li.innerText = `${pageNumber}---${i}`;
+  //       container.appendChild(li);
+  //     }
+
+  //     loopRender(currCnt - pageSize, ++pageNumber);
+  //   });
+  // }
+  // loopRender(total, pageNumber);
+
+  // 3. requestAnimationFrame + fragment 分片渲染
+  // let pageSize = 20;
+  // let pageNumber = 1;
+  // function loopRender(currCnt, pageNumber) {
+  //   if (currCnt < 0) return;
+  //   let pageCnt = Math.min(pageSize, currCnt);
+  //   requestAnimationFrame(() => {
+  //     let fragment = document.createDocumentFragment();
+  //     for (let i = 0; i < pageCnt; i++) {
+  //       let li = document.createElement("li");
+  //       li.innerText = `${pageNumber}---${i}`;
+  //       fragment.appendChild(li);
+  //     }
+  //     container.append(fragment);
+  //     loopRender(currCnt - pageSize, ++pageNumber);
+  //   });
+  // }
+  // loopRender(total, pageNumber);
+
+  // 4. requestAnimationFrame IntersectionObserver createDocumentFragment
+  const blank = document.querySelector(".blank");
+  let pageSize = 20;
+  let pageNumber = 1;
+  let totalPage = total / pageSize;
+  container.setAttribute("style", "height:100px;overflow:scroll");
+  function render(pageNumber) {
+    if (totalPage < pageNumber) return;
+    requestAnimationFrame(() => {
+      let fragment = document.createDocumentFragment();
+      for (
+        let i = pageNumber * pageSize;
+        i < pageNumber * pageSize + pageSize;
+        i++
+      ) {
+        let li = document.createElement("li");
+        li.innerText = `${pageNumber}---${i}`;
+        fragment.appendChild(li);
+      }
+      container.insertBefore(fragment, blank);
+    });
+  }
+  let Observer = new IntersectionObserver((entries) => {
+    if (entries[0].intersectionRatio > 0) {
+      render(pageNumber);
+      console.log("pageNumber", pageNumber);
+      pageNumber += 1;
+    }
+  });
+  Observer.observe(blank);
+});
--- 虚拟列表/index.js
+++ 虚拟列表/index.js
@@ -1,5 +1,7 @@
 const btn = document.querySelector("#button");
 const container = document.querySelector("#container");
+const list_all = document.querySelector(".list-all");
+const list_view = document.querySelector(".list-view");
 const total = 10000;
 const getData = () => {
   const list = [];
@@ -12,98 +14,43 @@ const getData = () => {
     }, 1000);
   });
 };
+const itemHeight = 20;
+let startIndex = 0,
+  endIndex = 20;
+
+const listAllHeight = total * itemHeight;
+list_all.style.height = listAllHeight + "px";
+const listViewCnt = 10;
+
+function render(listData) {
+  requestAnimationFrame(() => {
+    let fragment = document.createDocumentFragment();
+    for (let i = 0; i < listData.length; i++) {
+      let div = document.createElement("div");
+      div.className = "item";
+      div.innerText = `${i}`;
+      fragment.appendChild(div);
+    }
+    const itemList = document.querySelectorAll(".item");
+    let length = 10;
+    while (length) {
+      itemList[0] && list_view.removeChild(itemList[0]);
+      length--;
+    }
+    list_view.appendChild(fragment);
+  });
+}
 
 btn.addEventListener("click", async () => {
   const list = await getData();
-
-  // 1. 暴力直接一个一个渲染
-  // for (let i in list) {
-  //   let li = document.createElement("li");
-  //   li.innerText = i;
-  //   container.appendChild(li);
-  // }
-
-  // 2. setTimeout 分片渲染
-  // let pageSize = 20;
-  // let pageNumber = 1;
-  // function loopRender(currCnt, pageNumber) {
-  //   if (currCnt < 0) return;
-  //   let pageCnt = Math.min(pageSize, currCnt);
-  //   setTimeout(() => {
-  //     for (let i = 0; i < pageCnt; i++) {
-  //       let li = document.createElement("li");
-  //       li.innerText = `${pageNumber}---${i}`;
-  //       container.appendChild(li);
-  //     }
-
-  //     loopRender(currCnt - pageSize, ++pageNumber);
-  //   }, 0);
-  // }
-  // loopRender(total, pageNumber);
-
-  // 2. requestAnimationFrame 分片渲染
-  // let pageSize = 20;
-  // let pageNumber = 1;
-  // function loopRender(currCnt, pageNumber) {
-  //   if (currCnt < 0) return;
-  //   let pageCnt = Math.min(pageSize, currCnt);
-  //   requestAnimationFrame(() => {
-  //     for (let i = 0; i < pageCnt; i++) {
-  //       let li = document.createElement("li");
-  //       li.innerText = `${pageNumber}---${i}`;
-  //       container.appendChild(li);
-  //     }
-
-  //     loopRender(currCnt - pageSize, ++pageNumber);
-  //   });
-  // }
-  // loopRender(total, pageNumber);
-
-  // 3. requestAnimationFrame + fragment 分片渲染
-  // let pageSize = 20;
-  // let pageNumber = 1;
-  // function loopRender(currCnt, pageNumber) {
-  //   if (currCnt < 0) return;
-  //   let pageCnt = Math.min(pageSize, currCnt);
-  //   requestAnimationFrame(() => {
-  //     let fragment = document.createDocumentFragment();
-  //     for (let i = 0; i < pageCnt; i++) {
-  //       let li = document.createElement("li");
-  //       li.innerText = `${pageNumber}---${i}`;
-  //       fragment.appendChild(li);
-  //     }
-  //     container.append(fragment);
-  //     loopRender(currCnt - pageSize, ++pageNumber);
-  //   });
-  // }
-  // loopRender(total, pageNumber);
-
-  const blank = document.querySelector(".blank");
-  let pageSize = 20;
-  let pageNumber = 1;
-  let totalPage = total / pageSize;
-  container.setAttribute("style", "height:100px;overflow:scroll");
-  function render(pageNumber) {
-    if (totalPage < pageNumber) return;
-    requestAnimationFrame(() => {
-      let fragment = document.createDocumentFragment();
-      for (
-        let i = pageNumber * pageSize;
-        i < pageNumber * pageSize + pageSize;
-        i++
-      ) {
-        let li = document.createElement("li");
-        li.innerText = `${pageNumber}---${i}`;
-        fragment.appendChild(li);
-      }
-      container.insertBefore(fragment, blank);
-    });
-  }
-  let Observer = new IntersectionObserver((entries) => {
-    if (entries[0].intersectionRatio > 0) {
-      console.log("pageNumber", pageNumber);
-      pageNumber += 1;
-    }
+  let listData = list.slice(startIndex, endIndex);
+  container.addEventListener("scroll", (e) => {
+    let scrollTop = e.target.scrollTop;
+    startIndex = Math.floor(scrollTop / itemHeight);
+    endIndex = startIndex + listViewCnt;
+    listData = list.slice(startIndex, endIndex);
+    render(listData);
+    list_view.style.transform = `translate3d(0,${scrollTop},0)`;
   });
-  Observer.observe(blank);
+  render(listData);
 });
